65c7b147497bda06fe068f8cc9832bb3e14e9247
Daniel <danmilanes@gmail.com>
2023-11-28 16:58:45 -0500

    Added a check UI state that removes the filter and clear all button when no items are left on the list

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Variables to hold the elements from html file
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 // Add Item list
 function addItem(e) {
@@ -21,7 +23,11 @@ function addItem(e) {
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
+
   itemInput.value = "";
 }
 
@@ -43,10 +49,12 @@ function createIcon(classes) {
 
 // Remove item from list
 function removeItem(e) {
-  // console.log(e.target.parentElement.classList);
   if (e.target.parentElement.classList.contains("remove-item")) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm("Are you sure>")) {
+      e.target.parentElement.parentElement.remove();
+    }
   }
+  checkUI();
 }
 
 // Remove all items from list
@@ -54,9 +62,23 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
 }
 
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+
+checkUI();
